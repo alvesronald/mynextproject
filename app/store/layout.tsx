7473d7d394b6899ce7c58596b8673af05f67c8a6
alvesronald <alvesronaldtech@gmail.com>
2023-12-17 09:23:06 -0300

    feat: create sub-layout

---
+++ app/store/layout.tsx
@@ -0,0 +1,20 @@
+
+export default function StoreLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+    <nav>
+        <h3>Categories</h3>
+        <ul>
+            <li>
+                Books
+            </li>
+        </ul>
+    </nav>
+        {children}
+    </>
+  )
+}
