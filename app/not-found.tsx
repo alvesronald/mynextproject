6eef9ee2debe47133deb6625c14a9f31ae2167d4
alvesronald <alvesronaldtech@gmail.com>
2023-12-17 10:28:31 -0300

    feat: create 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function NotFound(){
+    return (
+        <section>
+            <h1>404</h1>
+            <p>Page not found.</p>
+
+            <Link href='/'>Voltar</Link>
+        </section>
+    )
+}
